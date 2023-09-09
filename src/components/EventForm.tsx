0b2600d9aacc7a43f99b4340e7d064488e70244d
Keeghen Kessler <kjrkessler@gmail.com>
2023-09-09 13:05:06 -0500

    Seperateration of concerns

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -1,27 +1,16 @@
 import React from "react";
 import { useDescriptionStore } from "../hooks/descriptionStore";
-import { useEventsStore } from "../hooks/eventsStore";
 import { useTitleStore } from "../hooks/titleStore";
-import { useValidationMessageStore } from "../hooks/validationMessageStore";
+import SaveEvent from "./SaveEvent";
 import DescriptionInput from "./DescriptionInput";
 import InputEnterButton from "./InputEnterButton";
 import TitleInput from "./TitleInput";
 
 const EventForm = () => {
-	const { changeMessage } = useValidationMessageStore();
-	const { addEvent } = useEventsStore();
 	const { title } = useTitleStore();
 	const { description } = useDescriptionStore();
 
-	const pressEnter = (title: string, description: string) => {
-		if (title.trim() && description.trim()) {
-			addEvent({ title, description });
-			changeMessage("Event saved.");
-		} else {
-			if (!title.trim()) changeMessage("Please enter valid title.");
-			else changeMessage("Please enter valid description.");
-		}
-	};
+	const pressEnter = SaveEvent();
 
 	return (
 		<form
