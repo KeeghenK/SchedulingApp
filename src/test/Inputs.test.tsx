cf7a5b63eca8cc9802b0bbf22f3ce8f8253d8174
Keeghen Kessler <kjrkessler@gmail.com>
2023-11-18 15:02:09 -0600

    Test if date input clears

--- src/test/Inputs.test.tsx
+++ src/test/Inputs.test.tsx
@@ -6,14 +6,21 @@ import App from "../components/App";
 describe("Input feilds", () => {
 	const testTitle = "Title";
 	const testDescription = "Description";
+	const testDate = "2023-01-01";
 
-	const saveEvent = (title: string, description: string) => {
+	const saveEvent = (
+		title: string = testTitle,
+		description: string = testDescription,
+		date: string = testDate
+	) => {
 		const titleInput = screen.getByTestId("test-title-input");
 		const descriptionInput = screen.getByTestId("test-description-input");
+		const dateInput = screen.getByTestId("test-date-input");
 		const inputButton = screen.getByTestId("test-event-form-button");
 
 		fireEvent.input(titleInput, { target: { value: title } });
 		fireEvent.input(descriptionInput, { target: { value: description } });
+		fireEvent.input(dateInput, { target: { value: date } });
 		fireEvent.click(inputButton);
 	};
 
@@ -26,7 +33,7 @@ describe("Input feilds", () => {
 			"test-title-input"
 		) as HTMLInputElement;
 
-		saveEvent(testTitle, testDescription);
+		saveEvent();
 
 		expect(titleInput.value).toBeFalsy();
 	});
@@ -36,8 +43,16 @@ describe("Input feilds", () => {
 			"test-description-input"
 		) as HTMLInputElement;
 
-		saveEvent(testTitle, testDescription);
+		saveEvent();
 
 		expect(descriptionInput.value).toBeFalsy();
+	});
+
+	it("should reset date input field if saved", () => {
+		const dateInput = screen.getByTestId("test-date-input") as HTMLInputElement;
+
+		saveEvent();
+
+		expect(dateInput.value).toBeFalsy();
 	});
 });
